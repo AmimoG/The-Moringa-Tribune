ebdaeea95ef54cd80e649363af78a0779ec02698
AmimoG <gilbertmatete6@gmail.com>
2020-10-12 08:40:38 +0300

    Add Files

--- news/views.py
+++ news/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from django.http  import HttpResponse,Http404
+from django.http import HttpResponse, Http404,HttpResponseRedirect
 import datetime as dt
 from .models import Article
 from .forms import NewsLetterForm
+from .email import send_welcome_email
 
 # Create your views here.
 def welcome(request):
@@ -11,11 +12,18 @@ def welcome(request):
 def news_of_day(request):
     date = dt.date.today()
     news = Article.todays_news()
-    
+
     if request.method == 'POST':
         form = NewsLetterForm(request.POST)
         if form.is_valid():
-            print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+
+            HttpResponseRedirect('news_today')
     else:
         form = NewsLetterForm()
     return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
@@ -68,3 +76,16 @@ def article(request,article_id):
     except DoesNotExist:
         raise Http404()
     return render(request,"all-news/article.html", {"article":article})
+
+def news_today(request):
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            HttpResponseRedirect('news_today')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
